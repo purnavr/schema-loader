519305c932fd884ad7643adfce97e3ffb3c07de0
purna <purnaraomba@gmail.com>
2024-09-02 14:15:29 +0530

    Roboshop Update

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+cd /etc/yum.repos.d/
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
 echo First Argument - $1
 echo Second Argument = $2
 
