6cd01d979aa47ad88320a6a855cca3eead232521
purna <purnaraomba@gmail.com>
2024-09-03 21:10:25 +0530

    Roboshop Update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM          dokken/centos-8:pr-81
+ENTRYPOINT    [ "bash", "/run.sh" ]
 RUN           yum install maven -y
 
 
--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
-echo First Argument - $1
-echo Second Argument = $2
+cd /etc/yum.repos.d/
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+
+
+
+#echo First Argument - $1
+#echo Second Argument = $2
 
 #git clone https://github.com/purnavr/$2.git /app
 #cd /app
